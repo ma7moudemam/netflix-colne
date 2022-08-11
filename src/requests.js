3b5a2dfcd3bc8ec26fd653a950b47442301aaf4e
m7moud emam <m7moud_emam@hotmail.com>
2022-08-12 00:33:54 +0200

    First Commit

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY ="4ec70b10c20ba7dc5e99b9286ea14306";
+
+const request ={
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/disover/movie?api_key=${API_KEY}&with_genres=99`
+}
+
+export default request;
